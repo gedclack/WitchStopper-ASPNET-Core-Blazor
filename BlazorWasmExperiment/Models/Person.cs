82bfbf690e4b6f5c2c7037f06a224933694f7126
Guz <agus_condro@penjahitsoftware.com>
2020-11-11 16:34:16 +0700

    Add project files.

---
+++ BlazorWasmExperiment/Models/Person.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWasmExperiment.Models
+{
+    public class Person
+    {
+        private int AgeOfDeath;
+        private int YearOfDeath;
+
+        public Person(int AgeOfDeath, int YearOfDeath)
+        {
+            this.AgeOfDeath = AgeOfDeath;
+            this.YearOfDeath = YearOfDeath;
+        }
+
+        public bool IsValid()
+        {
+            // rules:
+            // ~ value must be above 0
+            // ~ must born after the witch took control
+            if (AgeOfDeath > 0 && YearOfDeath > 0)
+            {
+                return GetBornYear() > 0;
+            }
+            return false;
+        }
+
+        public int GetBornYear()
+        {
+            return YearOfDeath - AgeOfDeath;
+        }
+    }
+}
